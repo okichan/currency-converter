dcf13ee50363168a440f8cf2b51d86c3c7ff3764
Tomomi <madeinwatashi@gmail.com>
2017-12-13 00:49:26 +1100

    Stats not loading

--- react-side/src/App.js
+++ react-side/src/App.js
@@ -11,8 +11,31 @@ class App extends Component {
 	onChangeHandler = (event) => {
 		this.setState({ userInput: event.target.value })
 		console.log(this.state.userInput);
-	}
-
+   }
+   
+   componentDidMount() {
+      this.onClickHandler() 
+      console.log("component mounted");
+   }
+   
+   onClickHandler() {
+      const {userInput} = this.state
+      getCurrency(userInput)
+         .then((stats) => {
+            this.setState({ stats: stats, error: null })
+            console.log("I'm from App.js");
+            console.log(this.state.stats);
+         }
+      )
+      .catch((error) => {
+         // If 404 not found
+         if (error.response.status === 404) {
+           error = new Error(`The stock symbol '${userInput}' does not exist`)
+         }
+         this.setState({ error: error })
+       })   
+   }
+   
 	
   render() {
 	 return (
@@ -22,7 +45,7 @@ class App extends Component {
 					<h1 className="text-warning text-center">Forex Calculator</h1>
 					<form className="form-search text-center" onChange={ this.onChangeHandler }>
 						<input type="text" className="input-medium search-query" placeholder={this.state.userInput} />
-						<button type="submit" className="btn">Search</button>
+						<button type="submit" className="btn" onClick={ () => 1+1 }>Search</button>
 					</form>
 					<hr/>
 				</div>
@@ -30,16 +53,31 @@ class App extends Component {
 					<h1 className="text-warning text-center">Latest Stats</h1>
 				</div>
 
+            {!!this.state.stats && (
+         
 				<div className="row-fulid"> 
-					<table className="table table-cell-hover" >
+					<table className="table table-cell-hover span4 offset2" style={{width: "70%", border: "1px solid red"}} >
 						<tbody>
-						<tr><td>Currency: </td></tr>
-						<tr><td>Date:</td></tr>
-						<tr><td>Currency:</td></tr>
-						<tr><td>Currency:</td></tr>
+						<tr>
+                     <td>Currency:</td> 
+                     <td>yay</td>
+                  </tr>
+						<tr>
+                     <td>Currency:</td> 
+                     <td>yay</td>
+                  </tr>
+						<tr>
+                     <td>Currency:</td> 
+                     <td>yay</td>
+                  </tr>
+						<tr>
+                     <td>Currency:</td> 
+                     <td>yay</td>
+                  </tr>
 						</tbody>
 					</table>
 				</div>
+            )}
 			</div>
 		</div>
 	 );
--- react-side/src/api-endpoint/CurrencyGetter.js
+++ react-side/src/api-endpoint/CurrencyGetter.js
@@ -6,5 +6,10 @@ const api = axios.create({
 
 export function getCurrency(symbol) {
   return api.get(`/latest?base=${symbol}`)
-    .then((res) => res.data)
+    .then((res) => {
+       res.data
+       console.log(`I'm from CurrencyGetter.js`);
+       console.log(res.data);
+      }
+   )
 }
