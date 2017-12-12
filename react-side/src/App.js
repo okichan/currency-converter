bc0b4bb4c84d65ba718bf9e9b51d4009a77c2d54
Tomomi <madeinwatashi@gmail.com>
2017-12-12 20:15:49 +1100

    Add Bootmetro

--- react-side/src/App.js
+++ react-side/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <div className="container"> 
+            <div className="row"> 
+               <h3><u>Different types of Headings</u></h3>
+               <h1>BootMetro!</h1>
+               <h2>BootMetro!</h2>
+               <h3>BootMetro!</h3>
+               <h4>BootMetro!</h4>
+               <h5>BootMetro!</h5>
+               <h6>BootMetro!</h6><br/>
+               <h3><u>Defining small text</u></h3>
+               <p><small>Welcome to BootMetro</small> </p><br/>
+               <h3><u>Different emphasis classes</u></h3>
+               <p className="text-warning">Welcome to BootMetro</p>
+               <p className="text-error">Welcome to BootMetro</p>
+               <p className="text-info">Welcome to BootMetro</p>
+               <p className="text-success">Welcome to BootMetro</p><br/>
+               <h3><u>Abbreviations</u></h3>
+               <abbr title="Transport Control Protocol">TCP</abbr>
+            </div>
+
+            <div>
+               <form className="form-search">
+                  <input type="text" className="input-medium search-query"/>
+                  <button type="submit" className="btn">Search</button>
+               </form>
+            </div>
+         </div>
       </div>
     );
   }
