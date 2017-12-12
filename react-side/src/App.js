0849e2e4596bdd02389ad92dd765bf89106dc4b6
Tomomi <madeinwatashi@gmail.com>
2017-12-12 20:33:19 +1100

    Add styling

--- react-side/src/App.js
+++ react-side/src/App.js
@@ -5,32 +5,19 @@ class App extends Component {
     return (
       <div className="App">
          <div className="container"> 
-            <div className="row"> 
-               <h3><u>Different types of Headings</u></h3>
-               <h1>BootMetro!</h1>
-               <h2>BootMetro!</h2>
-               <h3>BootMetro!</h3>
-               <h4>BootMetro!</h4>
-               <h5>BootMetro!</h5>
-               <h6>BootMetro!</h6><br/>
-               <h3><u>Defining small text</u></h3>
-               <p><small>Welcome to BootMetro</small> </p><br/>
-               <h3><u>Different emphasis classes</u></h3>
-               <p className="text-warning">Welcome to BootMetro</p>
-               <p className="text-error">Welcome to BootMetro</p>
-               <p className="text-info">Welcome to BootMetro</p>
-               <p className="text-success">Welcome to BootMetro</p><br/>
-               <h3><u>Abbreviations</u></h3>
-               <abbr title="Transport Control Protocol">TCP</abbr>
-            </div>
-
-            <div>
-               <form className="form-search">
-                  <input type="text" className="input-medium search-query"/>
+            <div className="row-fluid"> 
+               <h1 className="text-warning offset4">Hello</h1>
+               <form className="form-search  offset4">
+                  <input type="text" className="input-medium search-query" placeholder="..."/>
                   <button type="submit" className="btn">Search</button>
                </form>
             </div>
          </div>
+
+
+         
+
+         
       </div>
     );
   }
